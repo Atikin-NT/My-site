5fbdac8b83cc2071123bca58fee3b9749249a36c
Atikin-NT <ivanov.n.a.0620@gmail.com>
2021-12-21 23:49:53 +0300

    desing register

--- desing/static/js/custom.js
+++ desing/static/js/custom.js
@@ -1,3 +1,15 @@
+var listNameFornewArticle = ["article_title", "article_small_text", "article_small_text", "myfield"]
+
+for (let i =0; i < listNameFornewArticle.length; i++){
+    document.getElementById('form').querySelector(`[name="${listNameFornewArticle[i]}"]`).classList.add("form-control");
+    document.getElementById('form').querySelector(`[name="${listNameFornewArticle[i]}"]`).classList.add("form-control-custom");
+}
+
+// console.log(document.getElementByClass('markdownx-preview'))
+document.getElementsByClassName('markdownx-preview')[0].setAttribute("id", "content_det");
+// document.getElementByClass('markdownx-preview').classList.add("form-control-custom");
+
+
 var button = document.getElementsByClassName("login");
 var img_logo = document.getElementById("mainLogo");
 
